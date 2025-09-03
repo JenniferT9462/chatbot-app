007ac0ebf262d76ead4c634a476ae5968d3dd2eb
Jennifer Tarleton <jennifertarleton@gmail.com>
2025-09-03 00:32:58 -0500

    complete step 2 - UI and DOM wiring

--- chatbot.js
+++ chatbot.js
@@ -1,1 +1,16 @@
 console.log("Hello from chatbot.js");
+setText("chatbot-reply", "Test Message");
+
+onEvent("submit-btn", "click", function() {
+    console.log("Submit Button Clicked!");
+    const input = getValue("chatbot-input");
+    console.log(input);
+
+    if(input === "") {
+        setText("chatbot-reply", "⚠️ Please enter something!");
+        setProperty("chatbot-reply", "color", "red");
+    } else {
+        setText("chatbot-reply", "Thinking...");
+        setProperty("chatbot-reply", "color", "green");
+    }
+})
